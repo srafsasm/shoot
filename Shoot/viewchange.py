35ff86d97c645855f1aae83e529473f069bf71fb
srafsasm <71336788+srafsasm@users.noreply.github.com>
2022-12-04 18:47:30 +0900

    minor changes

--- Shoot/viewchange.py
+++ Shoot/viewchange.py
@@ -21,16 +21,15 @@ class View:
         self.start = start
 
     def chess_to_shoot(self):
-        if self.zchange >= 0.5: self.changing = False
         if self.changing:
-            gluLookAt(2.0*self.player+self.xchange, 0.0+self.ychange, 1.5-self.zchange,   0.0+self.xchange2, 0.0+self.ychange2, 0.0,   0.0, 0.0, 0.1)
+            gluLookAt(2.0*self.player+self.xchange, 0.0+self.ychange, 1.5-self.zchange,   0.0+self.xchange2, 0.0+self.ychange2, 0.0,   0.0, 0.0, 1)
             self.xchange += (self.start[0]-2.0*self.player)/10
             self.ychange += (self.start[1])/10
             self.xchange2 += (self.start[2])/10
             self.ychange2 += (self.start[3])/10
             self.zchange += 0.05
-        else:
-            gluLookAt(self.start[0], self.start[1], 1,   self.start[2], self.start[3], 0.0,   0.0, 0.0, 0.1)
+        if self.zchange >= 0.5: 
+            self.changing = False
             self.xchange = 0
             self.ychange = 0
             self.xchange2 = 0
@@ -39,7 +38,6 @@ class View:
 
 
     def shoot_to_chess(self):
-        if self.zchange >= 0.5: self.changing = False
         if self.changing:
             gluLookAt(self.start[0]+self.xchange, 0.0+self.start[1]+self.ychange, 1+self.zchange,   0.0+self.start[2]+self.xchange2, 0.0+self.start[3]+self.ychange2, 0.0,   0.0, 0.0, 0.1)
             self.xchange += (2.0*self.player-self.start[0])/10
@@ -49,6 +47,8 @@ class View:
             self.zchange += 0.05
         else:
             gluLookAt(2.0*self.player, 0.0, 1.5,   0.0, 0.0, 0.0,   0.0, 0.0, 0.1)
+        if self.zchange >= 0.5: 
+            self.changing = False
             self.xchange = 0
             self.ychange = 0
             self.xchange2 = 0
